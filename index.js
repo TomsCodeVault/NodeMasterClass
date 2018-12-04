696548058090481b4a646b089f6b2f56917bd3a5
OnlyNameUnused <32888916+OnlyNameUnused@users.noreply.github.com>
2018-12-04 17:39:29 -0500

    parse url

--- index.js
+++ index.js
@@ -5,16 +5,27 @@
 
 // Dependencies
 var http = require('http');
+var url = require('url');
 
 // The server should respond to all requests with a string
 var httpServer = http.createServer(function(req, res){
+  
+  // Parse the url from the requests
+  var parsedUrl = url.parse(req.url,true);
+  
+  // Get the path from the url and remove trailing /
+  var  path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+  
+  // Send response
   res.end('Hello World\n');
+  
+  // Log request path to console
+  console.log('Request received on path: '+trimmedPath);
+  
 });
 
 // Start the server, and have it listen on port 3000
 httpServer.listen(3000,function(){
   console.log('The server is listening on port 3000 now');
 });
-
-// Making edit to test git
-// Editing file from office pc
