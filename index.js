97996cdf45c80317bfc0c24e1c6ec7d761c59800
TomsCodeVault <tstoddard420@gmail.com>
2018-12-05 07:02:22 -0500

    index.js update to Return JSON

--- index.js
+++ index.js
@@ -31,16 +31,44 @@ var httpServer = http.createServer(function(req, res){
 	var decoder = new StringDecoder('utf-8');
 	var buffer = '';
 	req.on('data',function(data){
-		buffer += data;
+		buffer += decoder.write(data);
 	});
 	req.on('end',function(){
 		buffer += decoder.end();
-	
-		// Send response
-		res.end('Hello World\n');
-  
-		// Log request path to console
-		console.log('Request received with this payload: ',buffer);
+		
+		// Choose the handler this request should use. If one is not found use the notFound handler
+		var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+		
+		// Construct the data object to send to the handler
+		var data = {
+			'trimmedPath' : trimmedPath,
+			'queryStringObject' : queryStringObject,
+			'method' : method,
+			'headers' : headers,
+			'payload' : buffer
+		};
+		
+		// Route the request to the specified route
+		chosenHandler(data,function(statusCode,payload){
+			// Use the status code called back or default to 200
+			statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+			
+			// Use the payload called back or default to an empty object
+			payload = typeof(payload) == 'object' ? payload : {};
+			
+			// Convert the payload to a string
+			var payloadString = JSON.stringify(payload);
+			
+			// Return the response
+			res.setHeader('Content-Type','application/json');
+			res.writeHead(statusCode);
+			res.end(payloadString);
+
+			// Log request path to console
+			console.log('Returning this response: ',statusCode,payloadString);
+			
+		});  
+
 	});
   
 });
@@ -49,3 +77,22 @@ var httpServer = http.createServer(function(req, res){
 httpServer.listen(3000,function(){
   console.log('The server is listening on port 3000 now');
 });
+
+// Define handlers
+var handlers = {};
+
+// Hello handler
+handlers.hello = function(data,callback){
+	// Callback a http status code, and a payload object
+	callback(406,{'message' : 'welcome world!'});
+};
+
+// Not found handler
+handlers.notFound = function(data,callback){
+	callback(404);
+};
+
+// Define a request router
+var router = {
+	'hello' : handlers.hello
+};
